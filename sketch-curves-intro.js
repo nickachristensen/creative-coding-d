edd142859b1105c6491a43fbc21479c0ec1e81b9
Nicholas A Christensen <nickachristensen@gmail.com>
2022-11-22 21:41:49 -0500

    now able to move each point and manipulate the curve in real time

--- sketch-curves-intro.js
+++ sketch-curves-intro.js
@@ -1,15 +1,24 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true,
 };
 
-const sketch = () => {
-  const points = [
+let elCanvas;
+let points;
+
+const sketch = ({ canvas }) => {
+  points = [
     new Point({ x: 200, y: 540 }),
-    new Point({ x: 400, y: 300, control: true }),
+    new Point({ x: 400, y: 700, control: true }),
     new Point({ x: 880, y: 540 }),
   ];
+
+  canvas.addEventListener('mousedown', onMouseDown);
+
+  elCanvas = canvas;
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
@@ -25,6 +34,36 @@ const sketch = () => {
 
   };
 };
+
+const onMouseDown = (e) => {
+  window.addEventListener('mousemove', onMouseMove);
+  window.addEventListener('mouseup', onMouseUp);
+
+  const x = (e.offsetX / elCanvas.offsetWidth * elCanvas.width);
+  const y = (e.offsetY / elCanvas.offsetHeight * elCanvas.height);
+
+  points.forEach(point => {
+    point.isDragging = point.hitTest(x, y);
+  })
+}
+
+const onMouseMove = (e) => {
+  const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
+  const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
+
+
+  points.forEach(point => {
+    if (point.isDragging){
+      point.x = x;
+      point.y = y;
+    }
+  })
+}
+
+const onMouseUp = () => {
+  window.removeEventListener('mousemove', onMouseMove);
+  window.removeEventListener('mouseup', onMouseUp);
+}
 
 canvasSketch(sketch, settings);
 
@@ -45,5 +84,13 @@ class Point {
     context.fill();
 
     context.restore();
+  }
+
+  hitTest(x, y){
+    const dx = this.x - x;
+    const dy = this.y - y;
+    const dd = Math.sqrt(dx * dx + dy * dy)
+
+    return dd < 20;
   }
 }
