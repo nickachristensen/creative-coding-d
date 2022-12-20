e147f414eb6bc45105b6bb22a4c5bbe399850725
Nicholas A Christensen <nickachristensen@gmail.com>
2022-12-19 19:16:54 -0500

    animated

--- sketch-curves.js
+++ sketch-curves.js
@@ -4,7 +4,8 @@ const math = require('canvas-sketch-util/math');
 const colormap = require('colormap');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true,
 };
 
 const sketch = ({ width, height }) => {
@@ -40,8 +41,8 @@ const sketch = ({ width, height }) => {
     y = Math.floor(i / cols)* ch;
 
     n = random.noise2D(x, y, frequency, amplitude);
-    x += n;
-    y += n;
+    // x += n;
+    // y += n;
 
     lineWidth = math.mapRange(n, -amplitude, amplitude, 0, 5);
 
@@ -50,7 +51,7 @@ const sketch = ({ width, height }) => {
     points.push(new Point({ x, y, lineWidth, color }));
   }
 
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame }) => {
     context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
 
@@ -59,6 +60,13 @@ const sketch = ({ width, height }) => {
     context.translate(cw * 0.5, ch * 0.5);
     context.strokeStyle = 'red';
     context.lineWidth = 4;
+
+    //update position
+    points.forEach((point) => {
+      n = random.noise2D(point.ix + frame * 3, point.iy, frequency, amplitude);
+      point.x = point.ix + n;
+      point.y = point.iy + n;
+    })
 
     let lastx, lasty;
 
@@ -117,6 +125,9 @@ class Point {
     this.y = y;
     this.lineWidth = lineWidth;
     this.color = color;
+
+     this.ix = x;
+     this.iy = y;
   }
 
   draw(context) {
