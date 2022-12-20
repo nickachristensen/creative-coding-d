acee82df55342213b48c53e0d78e60907ac50f67
Nicholas A Christensen <nickachristensen@gmail.com>
2022-12-19 21:14:15 -0500

    audio sketch start

---
+++ sketch-audio.js
@@ -0,0 +1,27 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+let audio;
+
+const sketch = () => {
+  audio = document.createElement('audio');
+  audio.src = 'audio/ikoliks - Big City Lights.mp3';
+
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+  };
+};
+
+const addListeners = () => {
+  window.addEventListener('mouseup', () => {
+   if (audio.paused) audio.play();
+   else audio.pause();
+  })
+}
+
+addListeners();
+canvasSketch(sketch, settings);
